a4e3faa01fef06cf289f39684ad6a92e67849a7a
comougi <spsprwrw@gmail.com>
2020-12-12 11:17:34 +0300

    fix & cleanup

--- TaskFour.java
+++ TaskFour.java
@@ -3,59 +3,31 @@ package com.og;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.Scanner;
-import java.util.function.BiFunction;
+import java.util.function.Function;
 
-public class Main implements Comparable<com.og.Main> {
-    int number;
-
-    public Main(int number) {
-        this.number = number;
-    }
+public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String[] str = scanner.nextLine().split(" ");
 
-
-        Integer[] numbers = new Integer[str.length];
-
-
-        BiFunction<String[], Integer[], com.og.Main[]> sortByEvenAtFirst = (strings, ints) -> {
+        Function<String[], Integer[]> sortByEvenAtFirst = strings -> {
+            Integer[] ints = new Integer[strings.length];
             for (int i = 0; i < strings.length; i++) {
                 ints[i] = Integer.parseInt(strings[i]);
             }
             Arrays.sort(ints);
-            com.og.Main[] mainNumbers = new com.og.Main[str.length];
-            for (int i = 0; i < mainNumbers.length; i++) {
-                mainNumbers[i] = new com.og.Main(numbers[i]);
-            }
 
-            Arrays.sort(mainNumbers, new Comparator<com.og.Main>() {
+            Arrays.sort(ints, new Comparator<Integer>() {
                 @Override
-                public int compare(com.og.Main o1, com.og.Main o2) {
-                    return o1.compareTo(o2);
+                public int compare(Integer o1, Integer o2) {
+                    if (o2 % 2 != 0 && o1 % 2 == 0) return -1;
+                    return 0;
                 }
             });
-            return mainNumbers;
+            return ints;
         };
 
-
-        System.out.println(Arrays.toString(sortByEvenAtFirst.apply(str, numbers)));
-
-    }
-
-    public int getNumber() {
-        return number;
-    }
-
-    @Override
-    public String toString() {
-        return " " + number;
-    }
-
-    @Override
-    public int compareTo(com.og.Main other) {
-        return ((this.number % 2 != 0) ? 1 : (other.getNumber() % 2 == 0) ? 0 : -1);
+        System.out.println(Arrays.toString(sortByEvenAtFirst.apply(str)));
     }
 }
-
